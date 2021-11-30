73297bc7af287c32ef6214d680b1f024190df082
mayone-du <cocomayo1201@icloud.com>
2021-11-30 22:49:36 +0900

    add: comment

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use iced::{button, Button, Column, Element, Sandbox, Settings, Text};
 
+// 実行時に呼ばれるmain関数
 pub fn main() -> iced::Result {
     Counter::run(Settings::default())
 }
 
+// アプリケーションのstateを定義
 #[derive(Default)]
 struct Counter {
     value: i32,
@@ -11,6 +13,7 @@ struct Counter {
     decrement_button: button::State,
 }
 
+// ユーザーの操作によるメッセージ。イベントの定義。
 #[derive(Debug, Clone, Copy)]
 enum Message {
     IncrementPressed,
@@ -20,10 +23,12 @@ enum Message {
 impl Sandbox for Counter {
     type Message = Message;
 
+    // アプリケーションが実行されるときに呼ばれる関数。自分で明示的に呼ぶひつようはない
     fn new() -> Self {
         Self::default()
     }
 
+    // アプリケーションのタイトルを返す関数
     fn title(&self) -> String {
         String::from("Counter - Iced")
     }
@@ -39,14 +44,19 @@ impl Sandbox for Counter {
         }
     }
 
+    // アプリケーションのUIを描画する関数
     fn view(&mut self) -> Element<Message> {
+        // カラム
         Column::new()
             .padding(20)
+            // ボタンを生成し、クリックイベントにMessageのIncrementPressedを指定
             .push(
                 Button::new(&mut self.increment_button, Text::new("Increment"))
                     .on_press(Message::IncrementPressed),
             )
+            // カウントを表示
             .push(Text::new(self.value.to_string()).size(50))
+            // ボタンを生成し、クリックイベントにMessageのDecrementPressedを指定
             .push(
                 Button::new(&mut self.decrement_button, Text::new("Decrement"))
                     .on_press(Message::DecrementPressed),
